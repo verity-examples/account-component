c6dd4061cad321017b694e61621e10ea13a5f710
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2022-05-09 20:11:17 -0400

    Setup Postgres testing env

--- test/automated/account_component_test.exs
+++ test/automated/account_component_test.exs
@@ -3,6 +3,8 @@ defmodule AccountComponentTest do
   doctest AccountComponent
 
   test "greets the world" do
+    args = Application.fetch_env!(:account_component, Postgrex)
+    assert {:ok, _pid} = Postgrex.start_link(args)
     assert AccountComponent.hello() == :world
   end
 end
